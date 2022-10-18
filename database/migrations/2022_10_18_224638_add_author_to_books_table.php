c64bf4ebeceed0a3fbbe790e65617845c4dbfbf2
rauibert <rauibertnf@gmail.com>
2022-10-19 01:05:40 +0200

    Cambios varios en las migraciones

--- database/migrations/2022_10_18_224638_add_author_to_books_table.php
+++ database/migrations/2022_10_18_224638_add_author_to_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBooksTable extends Migration
+class AddAuthorToBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('ISBN');
-            $table->text('description');
-            $table->string('image');
-            $table->timestamps();
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('author')->nullable()->after('ISBN');
         });
     }
 
@@ -30,6 +25,8 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
     }
 }
